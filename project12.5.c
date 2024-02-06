72837b28c67601b1455a89bcfa451e1e6dc0a333
saurabhkachhadiya8 <156195401+saurabhkachhadiya8@users.noreply.github.com>
2024-02-06 17:49:55 +0530

    Add files via upload

--- project12.5.c
+++ project12.5.c
@@ -4,7 +4,7 @@ main(){
 	int i,j,k;
 	
 	for(i=1;i<=7;i++){
-		
+		if (i==5) continue;
 		for(k=6;k>=i;k--){
 			
 			printf(" ");
@@ -18,7 +18,7 @@ main(){
 		printf("\n");
 	}
 	for(i=6;i>=1;i--){
-		
+		if (i==5) continue;
 		for(k=i;k<=6;k++){
 			
 			printf(" ");
